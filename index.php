fea82c1c8cbf67811d04a9f765bb4e20190e4f25
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-10-28 21:37:39 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,201 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio</title>
+    <link rel="shortcut icon" href="#">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body class="vsc-initialized">
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    Portfolio
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">Recent</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    <small>Recent Work</small>
+                </h1>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/SYx3UCHZJlo">
+                    <img class="img-responsive" src="assets/images/1.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/SYx3UCHZJlo">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/o_rKNRoNz8s">
+                    <img class="img-responsive" src="assets/images/2.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/o_rKNRoNz8s">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/MS7KD9Ti7FQ">
+                    <img class="img-responsive" src="assets/images/3.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/MS7KD9Ti7FQ">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/1VYdx37vOGQ">
+                    <img class="img-responsive" src="assets/images/4.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/1VYdx37vOGQ">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/7yqyQQXgOT8">
+                    <img class="img-responsive" loading="lazy" src="assets/images/5.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/7yqyQQXgOT8">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="https://unsplash.com/photos/PfK35Iw1JAo">
+                    <img class="img-responsive" loading="lazy" src="assets/images/6.jpeg" alt="" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="https://unsplash.com/photos/PfK35Iw1JAo">Project Title</a>
+                </h3>
+                <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were multiply
+                    so sixth gathering fifth that man fowl made.</p>
+            </div>
+        </div>
+
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">«</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">»</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="footer-blurb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/images/7.jpeg" alt="" width="100" height="100">
+                        <h3>My Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="https://unsplash.com/photos/tNCH0sKSZbA">More Stuff</a></p>
+                    </div>
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/images/8.jpeg" alt="" width="100" height="100">
+                        <h3>Your Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="https://unsplash.com/photos/vkdOhd_oYic">More Stuff</a></p>
+                    </div>
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/images/9.jpeg" alt="" width="100" height="100">
+                        <h3>Our Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="https://unsplash.com/photos/VF4SWxE-MRw">More Stuff</a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <p>Copyright © Example.com 2021</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
