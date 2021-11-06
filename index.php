61fd25cc7fe69b1d3ae5a59f6aded092cda49b4f
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-11-05 20:13:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         <div class="row">
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/SYx3UCHZJlo">
-                    <img class="img-responsive" src="assets/images/1.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" src="assets/images/1.jpeg" srcset="media/555/1.jpeg  1024w, media/555/1.jpeg 640w, media/225/1.jpeg  320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/SYx3UCHZJlo">Project Title</a>
@@ -74,7 +74,7 @@
             </div>
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/o_rKNRoNz8s">
-                    <img class="img-responsive" src="assets/images/2.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" src="assets/images/2.jpeg" srcset="media/555/2.jpeg  1024w, media/555/2.jpeg 640w, media/225/2.jpeg 320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/o_rKNRoNz8s">Project Title</a>
@@ -88,7 +88,7 @@
         <div class="row">
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/MS7KD9Ti7FQ">
-                    <img class="img-responsive" src="assets/images/3.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" src="assets/images/3.jpeg" srcset="media/555/3.jpeg  1024w, media/555/3.jpeg 640w, media/225/3.jpeg 320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/MS7KD9Ti7FQ">Project Title</a>
@@ -98,7 +98,7 @@
             </div>
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/1VYdx37vOGQ">
-                    <img class="img-responsive" src="assets/images/4.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" src="assets/images/4.jpeg" srcset="media/555/4.jpeg  1024w, media/555/4.jpeg 640w, media/225/4.jpeg 320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/1VYdx37vOGQ">Project Title</a>
@@ -111,7 +111,7 @@
         <div class="row">
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/7yqyQQXgOT8">
-                    <img class="img-responsive" loading="lazy" src="assets/images/5.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" loading="lazy" src="assets/images/5.jpeg" srcset="media/555/5.jpeg  1024w, media/555/5.jpeg 640w, media/225/5.jpeg 320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/7yqyQQXgOT8">Project Title</a>
@@ -121,7 +121,7 @@
             </div>
             <div class="col-md-6 portfolio-item">
                 <a href="https://unsplash.com/photos/PfK35Iw1JAo">
-                    <img class="img-responsive" loading="lazy" src="assets/images/6.jpeg" alt="" width="555" height="370">
+                    <img class="img-responsive" loading="lazy" src="assets/images/6.jpeg" srcset="media/555/6.jpeg  1024w, media/555/6.jpeg 640w, media/225/6.jpeg 320w" width="555" height="370">
                 </a>
                 <h3>
                     <a href="https://unsplash.com/photos/PfK35Iw1JAo">Project Title</a>
@@ -166,21 +166,21 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/7.jpeg" alt="" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/7.jpeg" srcset="media/100/7.jpeg  1024w, media/100/7.jpeg 640w, media/100/7.jpeg 320w" width="100" height="100">
                         <h3>My Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
                         <p><a class="btn btn-primary" href="https://unsplash.com/photos/tNCH0sKSZbA">More Stuff</a></p>
                     </div>
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/8.jpeg" alt="" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/8.jpeg" srcset="media/100/8.jpeg  1024w, media/100/8.jpeg 640w, media/100/8.jpeg 320w" width="100" height="100">
                         <h3>Your Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
                         <p><a class="btn btn-primary" href="https://unsplash.com/photos/vkdOhd_oYic">More Stuff</a></p>
                     </div>
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/9.jpeg" alt="" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/9.jpeg" srcset="media/100/9.jpeg  1024w, media/100/9.jpeg 640w, media/100/9.jpeg 320w" width="100" height="100">
                         <h3>Our Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
