8b3ed7490b7691c24db5fe81d5397b32383ea3cf
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-11-05 18:51:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
     <title>Portfolio</title>
     <link rel="shortcut icon" href="#">
     <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/bootstrap.css" rel="preload" as="style" onload="this.rel='stylesheet'">
-    <link href="assets/css/style.css" rel="alternate stylesheet" type="text/css">
-    <link href="assets/css/style.css" rel="preload" as="style" onload="this.rel='stylesheet'">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body class="vsc-initialized">
