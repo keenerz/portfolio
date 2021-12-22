2cbb21f4fe0c5754cc150154860456b5a52daa7d
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-22 12:24:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -157,21 +157,21 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/7.jpeg" srcset_placeholder="media/100/7.jpeg  1024w, media/100/7.jpeg 640w, media/100/7.jpeg 320w" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/tNCH0sKSZbA.jpeg" srcset_placeholder="media/100/tNCH0sKSZbA.jpeg  1024w, media/100/tNCH0sKSZbA.jpeg 640w, media/100/tNCH0sKSZbA.jpeg 320w" width="100" height="100">
                         <h3>My Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
                         <p><a class="btn btn-primary" href="https://unsplash.com/photos/tNCH0sKSZbA">More Stuff</a></p>
                     </div>
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/8.jpeg" srcset_placeholder="media/100/8.jpeg  1024w, media/100/8.jpeg 640w, media/100/8.jpeg 320w" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/vkdOhd_oYic.jpeg" srcset_placeholder="media/100/vkdOhd_oYic.jpeg  1024w, media/100/vkdOhd_oYic.jpeg 640w, media/100/vkdOhd_oYic.jpeg 320w" width="100" height="100">
                         <h3>Your Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
                         <p><a class="btn btn-primary" href="https://unsplash.com/photos/vkdOhd_oYic">More Stuff</a></p>
                     </div>
                     <div class="col-sm-4 footer-blurb-item">
-                        <img class="img-circle" loading="lazy" src="assets/images/9.jpeg" srcset_placeholder="media/100/9.jpeg  1024w, media/100/9.jpeg 640w, media/100/9.jpeg 320w" width="100" height="100">
+                        <img class="img-circle" loading="lazy" src="assets/images/VF4SWxE-MRw.jpeg" srcset_placeholder="media/100/VF4SWxE-MRw.jpeg  1024w, media/100/VF4SWxE-MRw.jpeg 640w, media/100/VF4SWxE-MRw.jpeg 320w" width="100" height="100">
                         <h3>Our Stuff</h3>
                         <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
                             multiply so sixth gathering fifth that man fowl made.</p>
