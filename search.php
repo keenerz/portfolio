04d0ce35d1102a048a735f5eba7fc71b69e96323
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2022-01-15 11:30:25 -0800

    Add files via upload

--- search.php
+++ search.php
@@ -1,7 +1,25 @@
 <?php 
-    require_once("includes/classes/Form_Provider.php");
-    require_once("includes/configuration.php");  
+    require_once("includes/classes/Search_Grid.php");
     require_once("includes/header.php"); 
+
+    if (isset($_POST["search_input"])) {
+        $keywords = $_POST["search_input"];
+        $images = get_recent_images($keywords);
+        $images = json_decode($images);
+        $images = array_slice($images, 0, 9);
+        $grid = new Search_Grid();
+        $html = $grid -> create($images);
+    }
+
+    function get_recent_images($keywords) {
+        $request_url = "http://app.squidproquo.io/search?keywords=" . $keywords;
+        $curl = curl_init($request_url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $images = curl_exec($curl);
+        curl_close($curl);
+        return $images;
+    }
+
 ?>
 
         <div class="row">
@@ -12,12 +30,72 @@
             </div>
         </div>
         <div class="row">
+            <form action="search.php" method="POST" name="search_form" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label>Keywords:</label>
+                    <input type="text" class="form-control" name="search_input" value="<?php echo $keywords; ?>">
+                </div>
+                <div class="form-group" style="display: inline-flex;">
+                    <button type="submit" class="btn btn-primary" name="search_button" value="">Seach</button>
+                    
+                </div>
+            </form>
+        </div>
+        <div class="row">
             <?php
-                $form_provider = new Form_Provider($connection);
-                echo $form_provider -> create_search_form(); 
+                echo $html;
             ?>
         </div>
 
+        <script>
+
+            (async function() {
+
+                function toggle_submit_button() {
+                    let button = document.querySelector("button[name=search_button]");
+                    if (document.querySelector("input[name=search_input]").value == "") {
+                        button.disabled = true;
+                    }
+                    else {
+                        button.disabled = false;
+                    }
+                }
+
+                function display_spinner() {
+                    let button = document.querySelector("button[name=search_button]");
+                    let spinner = document.createElement("div");
+                        spinner.className = "spinner-border";
+                        spinner.setAttribute("role", "status");
+                        spinner.innerHTML = "<span class='sr-only'>Loading...</span>";
+                        button.after(spinner);
+                }
+
+                function disable_search() {
+                    let button = document.querySelector("button[name=search_button]");
+                        button.disabled = true;    
+                    let input = document.querySelector("input[name=search_input]");
+                        input.readOnly = true;
+                }
+
+                toggle_submit_button();
+
+                document.querySelector("input[name=search_input]").onkeyup = function(event) {
+                    if (event.key != "Enter") {
+                        toggle_submit_button();
+                    }
+                }
+
+                document.querySelector("form[name=search_form]").onsubmit = function() {
+                    display_spinner();
+                    disable_search();
+                }
+
+            })();
+
+        </script>
+
 <?php 
+
     require_once("includes/footer.php"); 
+
 ?>
