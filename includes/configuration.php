bf7be1afc281fa0905304d878c7e25e5da889a39
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2022-01-15 11:42:16 -0800

    Add files via upload

--- includes/configuration.php
+++ includes/configuration.php
@@ -1,20 +1,25 @@
 <?php
 
+    require_once("credentials.php");
+
     date_default_timezone_set("America/Los_Angeles");
 
     # attempts to execute specified code
     try {
     
         # specifies information to connect to database
-        $database = "#database_placeholder#";
-        $host = "#host_placeholder#";
-        $port = "3306";
-        $character_set = "utf8";
-        $user = "#user_placeholder#";
-        $password = "#password_placeholder#";
+        $database = MYSQL_DATABASE;
+        $host = MYSQL_HOST;
+        $port = MYSQL_PORT;
+        $charset = MYSQL_CHARSET;
+        $user = MYSQL_USER;
+        $password = MYSQL_PASSWORD;
+        $options = array(
+            #pdo_options_placeholder#
+        );
 
         # connects to database
-        $connection = new PDO("mysql:dbname=$database;host=$host;port=$port;charset=$character_set", $username, $password);
+        $connection = new PDO("mysql:dbname=$database;host=$host;port=$port;charset=$charset", $user, $password, $options);
         $connection -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     
